9415e89cbd45bc50601ffd9b0a7d5756e6cc5f57
masterlangueetinformatique <masterlangueetinformatique@vbox>
2024-10-24 10:28:24 +0200

    TD2, Amina, class A

---
+++ A.java
@@ -0,0 +1,16 @@
+package TD2;
+
+public class A {
+	/** test si un nombre est divisible par 13
+	 * 
+	 * @param n entier 
+	 * @return true si n est divisible par 13 false sinon
+	 */
+	
+	static boolean treize(int n) {
+		if (n%13==0) return true;
+		else return false;
+				
+	}
+
+}
