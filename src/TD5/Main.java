e9f75760fd0f6115a88e47adb009ee4f37f1cba4
masterlangueetinformatique <masterlangueetinformatique@vbox>
2024-11-11 12:53:04 +0100

    Exercice 2, TD5 (class et méthode d'un étudiant)

---
+++ src/TD5/Main.java
@@ -0,0 +1,12 @@
+package TD5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		etudiant Cleo= new etudiant(21101279,"Bibi Cleo",20,"évolution des escargots");
+		
+		Cleo.Promotion();
+
+	}
+
+}
--- src/TD5/etudiant.java
+++ src/TD5/etudiant.java
@@ -3,8 +3,23 @@ package TD5;
 public class etudiant {
 	/** attribut d'etudiant*/
 	int numero_etudiant;
-	char nom_prenom;
+	String nom_prenom;
 	int age;
-	char formation;
+	String formation;
+	
+	/** constructeur*/
+	public etudiant(int numero_etudiant, String nom_prenom,int age,String formation) {
+		this.numero_etudiant= numero_etudiant;
+		this.nom_prenom= nom_prenom;
+		this.age= age;
+		this.formation= formation;			
+	}
+	public void afficherEtudiant() {
+		System.out.println("n° étudiant: "+ numero_etudiant);
+		System.out.println("nom et prénom: "+ nom_prenom);
+		System.out.println("âge: "+ age);
+		System.out.println("Votre formation: "+ formation);
+	}
+	
 
 }
