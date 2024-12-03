a3d09450b76e6689316309d6100c34af99693315
amina200003 <aminaahamada124@gmail.com>
2024-12-03 16:52:01 +0100

    un entrainement pour Sur classe et sous classe (Vegetal -> fleur -> hibiscus -> test_fleur)

--- src/TD6/Hibiscus.java
+++ src/TD6/Hibiscus.java
@@ -5,9 +5,14 @@ public class Hibiscus extends fleur {
 		super(espece,petals,size_tige);
 		
 	}
-	
+	@Override 
+	public int getRespiration_cellulaire() {
+		return 10;
+	}
 	public void presentation() {
 		super.presentation();
+		System.out.println("Mon taux de CO2 est de"+getRespiration_cellulaire());
+		
 	}
 	
 
---
+++ src/TD6/test_vegetal.java
@@ -0,0 +1,10 @@
+package TD6;
+
+public class test_vegetal {
+public static void main (String[] args) {
+	Hibiscus maFleur= new Hibiscus("sabdariffa",6,13);
+	maFleur.presentation();
+	
+}
+
+}
