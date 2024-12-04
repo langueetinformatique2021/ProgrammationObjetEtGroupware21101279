36767fce438560e55744a020a00096493403af36
amina200003 <aminaahamada124@gmail.com>
2024-12-04 21:42:32 +0100

    entrainement sur-classe et sous-classe, ajout d'une interface "dormir" (vegetal -> fleur -> hibiscus)

--- src/TD6/Hibiscus.java
+++ src/TD6/Hibiscus.java
@@ -1,18 +1,22 @@
 package TD6;
 
-public class Hibiscus extends fleur {
+public class Hibiscus extends fleur implements dormir {
+	private int heureDodo= 4;
 	// constructeurs de hibiscus
 	public Hibiscus(String espece,int petals,int size_tige) {
 		super(espece,petals,size_tige); // ref des attribut de sur class fleur et class au-dessus superVegetal 
-		
 	}
 	@Override 
 	public int getRespiration_cellulaire() {
 		return 10; // modification de respiration
 	}
+	
+	public int heureSommeil() {
+		return heureDodo;
+	}
 	public void presentation() {
 		super.presentation(); // ref de cette methode ds fleur ET ds superVegetal 
-		System.out.println("Mon taux de CO2 est de"+getRespiration_cellulaire()); //+ modofication
+		System.out.println("Mon taux de CO2 est de"+getRespiration_cellulaire() + "et je dors pdt" + heureDodo); //+ modofication
 		
 	}
 	
---
+++ src/TD6/dormir.java
@@ -0,0 +1,6 @@
+package TD6;
+// tout ce qui est ds interface est assumer comme etant abstract
+public interface dormir {
+	public  int heureSommeil();
+
+}
