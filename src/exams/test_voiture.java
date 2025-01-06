ac1e8b5ffe98c753928821d58358b773cee68e8a
amina200003 <aminaahamada124@gmail.com>
2025-01-06 12:58:03 +0100

    training heritage(voiture)

--- src/exams/test_voiture.java
+++ src/exams/test_voiture.java
@@ -1,7 +1,7 @@
 package exams;
 
 public class test_voiture {
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		Car Lili= new Car("Bibi",2012,4);
 		Car Shou= new Car("Bu",2007,6);
 		Lili.Info();
